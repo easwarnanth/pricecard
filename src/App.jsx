1f727d37e21de93871aa9a888a1e4feda9dd79e6
easwarnanth <easwarnanthu0@gmail.com>
2024-04-09 13:29:14 +0530

    Reacr-pricecard

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import CompA from './components/price.jsx';
+import"./components/price.css"
+
+const App = () => {
+  const arrObj = [
+    {
+      title: "FREE",
+      price: "$10/month",
+      user: "single user",
+      project: "text-muted",
+      proIcon: "fa-solid fa-regular fa-x",
+      support: "text-muted",
+      supIcon: "fa-solid fa-regular fa-x",
+      domain: "text-muted",
+      domIcon: "fa-solid fa-regular fa-x",
+      report: "text-muted",
+      repIcon: "fa-solid fa-regular fa-x",
+
+    },
+    {
+      title: "PLUS",
+      price: "$9/month",
+      user: "5 user",
+      project: "",
+      proIcon: "fa-solid fa-check",
+      support: "",
+      supIcon: "fa-solid fa-check",
+      domain: "",
+      domIcon: "fa-solid fa-check",
+      report: "text-muted",
+      repIcon: "fa-solid fa-regular fa-x",
+
+    },
+    {
+      title: "PRO",
+      price: "$49/month",
+      user: "unlimited user",
+      project: "",
+      proIcon: "fa-solid fa-check",
+      support: "",
+      supIcon: "fa-solid fa-check",
+      domain: "",
+      domIcon: "fa-solid fa-check",
+      report: "",
+      repIcon: "fa-solid fa-check",
+    }
+
+  ]
+
+  return (
+    <section className='price p-5'>
+      <div className='container'>
+
+        <CompA arrObj={arrObj} />
+
+
+
+      </div>
+    </section>
+
+
+  );
+};
+
+export default App;
